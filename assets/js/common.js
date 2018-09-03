3c38733a7457b8baf9d47000c0ee4d3758bdc4e9
nicolssx <nicolssx@163.com>
2018-09-03 10:59:10 +0800

    hexo博客构建测试

--- assets/js/common.js
+++
@@ -1,7 +0,0 @@
-
-if(navigator.serviceWorker){
-  console.log('true');
-  navigator.serviceWorker.register('sw.js').then(function(registration){
-    console.log('Registered enevt at scope:',registration.scope);
-  })
-}
